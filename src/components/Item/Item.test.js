9598461a60dca8371484a923d3e4a915b887f70a
Guilherme Garcia <gui.garcia67@gmail.com>
2022-01-21 01:13:30 -0300

    Adds Item.js component testing

---
+++ src/components/Item/Item.test.js
@@ -0,0 +1,35 @@
+import {
+    render,
+    screen,
+    fireEvent,
+    waitFor,
+} from '@testing-library/react';
+import { Item } from './Item';
+
+test('Fires the EDIT function when edit button is clicked', async () => {
+    const item = { note: 'Now go', date: '20/01/2022 16:27', id: 1 };
+
+    const editFn = jest.fn();
+
+    render(<Item item={item} editFn={editFn} />);
+
+    const editBtn = screen.getByTestId(`edit-btn-${item.id}`);
+
+    fireEvent.click(editBtn);
+
+    expect(editFn).toHaveBeenCalledWith(item);
+});
+
+test('Fires the DELETE function when delete button is clicked', async () => {
+    const item = { note: 'Now go', date: '20/01/2022 16:27', id: 1 };
+
+    const deleteFn = jest.fn();
+
+    render(<Item item={item} deleteFn={deleteFn} />);
+
+    const deletetBtn = screen.getByTestId(`delete-btn-${item.id}`);
+
+    fireEvent.click(deletetBtn);
+
+    expect(deleteFn).toHaveBeenCalledWith(item);
+});
